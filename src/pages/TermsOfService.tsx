e4aa50c21823707b17382b39c14563bb4e56f1f0
Nicholas King <449071+viralarchitect@users.noreply.github.com>
2025-10-24 21:27:02 -0500

    Remove unused ExternalLink import from TermsOfService

--- src/pages/TermsOfService.tsx
+++ src/pages/TermsOfService.tsx
@@ -1,5 +1,4 @@
 import { Link } from 'react-router-dom';
-import { ExternalLink } from '@/components/ui/external-link';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 
